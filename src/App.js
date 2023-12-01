1daa9ec81445c07df209d302d8f67a04ff0fb7ed
spathiyal <spathiyal@gmail.com>
2023-12-01 15:57:29 -0800

    removed component and render

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import React, { Component } from "react";
+import React from "react";
 import JokeList from "./JokeList";
 
-/** App component. Renders list of jokes. */
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <JokeList />
-      </div>
-    );
-  }
+/** App component. return list of jokes. */
+//removed component and render
+function App() {
+  return (
+    <div className="App">
+      <JokeList />
+    </div>
+  );
 }
 
 export default App;
